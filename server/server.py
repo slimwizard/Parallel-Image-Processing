98d3c20dc7eb77d39f33b0d4d1c168d9162d0ece
cperez941 <carlp941@gmail.com>
2018-11-01 20:23:44 -0500

    going around at the speed of sound

--- server/server.py
+++ server/server.py
@@ -29,9 +29,8 @@ def upload_file():
         if file_uploaded and allowed_file(file_uploaded.filename):
             file_uploaded.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             uploaded_file_url = str(request.base_url) + "/" + str(filename)
-            test = send_to_googlenet(uploaded_file_url)
-            return str(test)
-            #return "{0} successfully uploaded.".format(filename)
+            probabilities = send_to_googlenet(uploaded_file_url)
+            return str(probabilities)
 
 # redirect to uploaded file
 @app.route('/upload/<filename>')
