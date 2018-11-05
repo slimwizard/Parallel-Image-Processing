993bd6431d2e248b720ea8bd2d97f77a890d88b3
Grant <g.free697@gmail.com>
2018-11-05 15:46:57 -0600

    memory usage now functional. check comment in metrics.sh for usage

--- metrics/metrics.sh
+++ metrics/metrics.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# run using ./metrics.sh [filename] [time to collect]
+# run using ./metrics.sh [filename{EXCLUDING FILE EXTENSION}] [polling time]
 
-sar -u 1 $2 | awk -f sar_2_csv.awk > $1 &
+sar -u 1 $2 | awk -f sar_2_csv.awk > cpu_$1.csv &
+sar -r 1 $2 | awk -f sar_2_csv.awk > mem_$1.csv &
 
 python3 example_metrics.py
