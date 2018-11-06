f93f8a69b7807b4075456e2f90873fdda6c80014
Grant <g.free697@gmail.com>
2018-11-05 18:24:08 -0600

    added control over polling interval

--- metrics/metrics.sh
+++ metrics/metrics.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# run using ./metrics.sh [filename{EXCLUDING FILE EXTENSION}] [polling time]
+# run using ./metrics.sh [filename{EXCLUDING FILE EXTENSION}] [poll interval] [polling time]
 
-sar -u 1 $2 | awk -f sar_2_csv.awk > cpu_$1.csv &
-sar -r 1 $2 | awk -f sar_2_csv.awk > mem_$1.csv &
+sar -u $2 $3 | awk -f sar_2_csv.awk > cpu_$1.csv &
+sar -r $2 $3 | awk -f sar_2_csv.awk > mem_$1.csv &
 
 python3 example_metrics.py
