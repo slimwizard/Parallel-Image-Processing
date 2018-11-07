9fb0759d7dea49fdbe021f66adcf043f5111a801
philiptkd <philiptkd1@gmail.com>
2018-11-06 23:29:59 -0600

    final state after testing with one worker

--- metrics/metrics.sh
+++ metrics/metrics.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # run using ./metrics.sh [filename{EXCLUDING FILE EXTENSION}] [poll interval] [polling iterations] [file to run]
+# call from directory expected by [file to run]
 
 sar -u $2 $3 | awk -f sar_2_csv.awk > cpu_$1.csv &
 sar -r $2 $3 | awk -f sar_2_csv.awk > mem_$1.csv &
