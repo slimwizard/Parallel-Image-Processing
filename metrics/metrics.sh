77125a113b582beb17ef10791a87a8f71d1d7da2
Grant <g.free697@gmail.com>
2018-11-10 15:56:33 -0600

    fixed string eq

--- metrics/metrics.sh
+++ metrics/metrics.sh
@@ -2,10 +2,10 @@
 # run using ./metrics.sh [filename{EXCLUDING FILE EXTENSION}] [poll interval] [polling iterations] [file to run] [wlan|eth] [pc|pi]
 # call from directory expected by [file to run]
 
-if [$5 == "wlan"]; then
+if [[ "$5" = "wlan" ]]; then
 	sar -u $2 $3 | awk -f sar_2_csv.awk > cpu_$1.csv &
 	sar -r $2 $3 | awk -f sar_2_csv.awk > mem_$1.csv &
-	if [$6 == "pi"]; then
+	if [[ "$6" = "pi" ]]; then
 		sar -n DEV $2 $3 | awk -f sar_net_2_csv_pi_wlan.awk > net_$1.csv &
 	else
 		sar -n DEV $2 $3 | awk -f sar_net_2_csv_pc_wlan.awk > net_$1.csv &
@@ -13,7 +13,7 @@ if [$5 == "wlan"]; then
 else
 	sar -u $2 $3 | awk -f sar_2_csv.awk > cpu_$1.csv &
 	sar -r $2 $3 | awk -f sar_2_csv.awk > mem_$1.csv &
-	if [$6 == "pi"]; then
+	if [[ "$6" = "pi" ]]; then
 		sar -n DEV $2 $3 | awk -f sar_net_2_csv_pi_eth.awk > net_$1.csv &
 	else
 		sar -n DEV $2 $3 | awk -f sar_net_2_csv_pc_eth.awk > net_$1.csv &
