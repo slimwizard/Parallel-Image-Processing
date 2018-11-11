30faedd3ba06de4b87da66e09b19e6732b49673a
philiptkd <philiptkd1@gmail.com>
2018-11-11 00:04:44 -0600

    trying a scope to resolve naming issues

--- server/googlenet/dist_googlenet.py
+++ server/googlenet/dist_googlenet.py
@@ -62,10 +62,16 @@ def build_graph(cluster, image_url, return_list):
     sess = tf.Session(target=server.target)
 
     # Create the model, use the default arg scope to configure the batch norm parameters.
-    with slim.arg_scope(inception.inception_v1_dist_arg_scope()):
-        with tf.device(tf.train.replica_device_setter(cluster=cluster)):
-            logits, _ = inception.inception_v1_dist(shared_image, num_classes=1001, is_training=False)
-            probabilities = tf.nn.softmax(logits)
+    with tf.variable_scope("sharing", reuse=True):
+        with slim.arg_scope(inception.inception_v1_dist_arg_scope()):
+            with tf.device(tf.train.replica_device_setter(cluster=cluster)):
+                logits, _ = inception.inception_v1_dist(shared_image, num_classes=1001, is_training=False)
+                probabilities = tf.nn.softmax(logits)
+
+    # TROUBLESHOOTING
+    #print(tf.get_default_graph().get_operations())
+    #return
+    # END TROUBLESHOOTING    
 
     # initialization function that uses saved parameters
     init_fn = slim.assign_from_checkpoint_fn(
