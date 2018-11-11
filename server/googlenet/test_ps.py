3a74733ca5faaace8d0f00b6f7d229c27fc4dc1b
philiptkd <philiptkd1@gmail.com>
2018-11-10 22:11:09 -0600

    added discovery to test_ps.py as well

--- server/googlenet/test_ps.py
+++ server/googlenet/test_ps.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import configparser
 import os
 cwd = os.getcwd()
 
@@ -6,18 +7,31 @@ TESTING = False
 
 if cwd[-len("googlenet"):] == "googlenet": # for calling this file as the entry point, from this directory
     import dist_googlenet as dg
+    ini_path = "../../node_code/ps_worker.ini"
     TESTING = True
 else:
     import googlenet.dist_googlenet as dg # for calling server.py as the entry point, from the server directory
+    ini_path = "../node_code/ps_worker.ini"
 
 def pass_url_to_graph(image_url, return_list):
-    cluster = tf.train.ClusterSpec({
-            "worker": [ "192.168.0.2:2222"#,
-                        #"192.168.86.176:2222",
-                        #"192.168.86.172:2222"
-                      ],
-            "ps": ["192.168.0.6:2222"]
-            })
+    config = configparser.ConfigParser()
+    config.read(ini_path)
+    workers_listing = []
+    ps_listing = []
+
+    for worker in config["IP Listing"]["worker"].split(", "):
+        worker = str(worker) + ":2222"
+        workers_listing.append(worker)
+
+    for ps in [config["IP Listing"]["ps"]]:
+        ps = str(ps) + ":2222"
+        ps_listing.append(ps)
+
+    jobs = { "worker" : workers_listing,
+             "ps" : ps_listing
+    }
+    cluster = tf.train.ClusterSpec(jobs)
+    
     dg.build_graph(cluster, image_url, return_list)
 
 if TESTING:
